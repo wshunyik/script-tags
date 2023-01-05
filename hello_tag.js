4163ee5edba1b72aaf9ec09406cf026e1aa36b8b
Divij Lankalapalli <ldivij@divijs-mbp.myfiosgateway.com>
2023-01-04 23:14:12 -0500

    switched to cdns, testing....

--- hello_tag.js
+++ hello_tag.js
@@ -1,10 +1,2 @@
 var my_var=true;
 console.log('Hello world');
-const yourFunction = async () => {
-    await delay(5000);
-    console.log("Waited 5s");
-  
-    await delay(5000);
-    console.log("Waited an additional 5s");
-  };
-  
